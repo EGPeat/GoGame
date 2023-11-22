6f219ae2a3ae096546570d70063857cc5fc2de6a
Evan Peatman <evanpeatman@gmail.com>
2023-11-22 13:21:13 +1030

    starting on making class HexGo()

--- pygametest.py
+++ pygametest.py
@@ -6,8 +6,9 @@ import PySimpleGUI as sg
 import os
 import platform
 
-RADIUS = 80
-CIRC_RAD = 20
+RADIUS = 60
+CIRC_RAD = 15
+BOARD_SIZE = 6
 
 
 class HexNode():
@@ -31,33 +32,42 @@ class HexNode():
             print("No attribute for that")
 
 
+class HexGo(): #make it inherit from the other go class lol...
+    def __init__(self, radius, circ_rad, board_size, window):
+        self.radius = radius
+        self.circ_rad = circ_rad
+        self.board_size = board_size
+        self.window = window
+
 # longest one for a board of sidelength 6 is 11 (6*2-1)
 
-def generate_board(side_length, start_point):
+def generate_board(side_length, start_point, board_array):
     nodes_set = set()
     nodes_dict = dict()
-    generate_row(side_length - 1, (side_length * 2) - 1, start_point, nodes_set, nodes_dict, False)
-    generate_board_helper(side_length, -30, True, nodes_set, nodes_dict, start_point)
-    generate_board_helper(side_length, 150, False, nodes_set, nodes_dict, start_point)
+    generate_row(side_length - 1, (side_length * 2) - 1, start_point, nodes_set, nodes_dict, board_array, False)
+    generate_board_helper(side_length, -30, True, nodes_set, nodes_dict, start_point, board_array)
+    generate_board_helper(side_length, 150, False, nodes_set, nodes_dict, start_point, board_array)
     return (nodes_set, nodes_dict)
 
 
-def generate_board_helper(side_length, angle_deg, up, nodes_set, nodes_dict, start_point):
+def generate_board_helper(side_length, angle_deg, up, nodes_set, nodes_dict, start_point, board_array):
     for idx in range(1, side_length):
         angle_rad = math.pi / 180 * angle_deg
         y_val = start_point[1] - idx * RADIUS * math.cos(angle_rad)
         if up:
             x_val = start_point[0] - idx * RADIUS * math.sin(angle_rad)
-            generate_row((side_length - 1) - idx, (side_length * 2) - 1 - idx, (x_val, y_val), nodes_set, nodes_dict, True, True)
+            generate_row((side_length - 1) - idx, (side_length * 2) - 1 - idx, (x_val, y_val), nodes_set, nodes_dict, board_array, True, True)
         if not up:
             x_val = start_point[0] + idx * RADIUS * math.sin(angle_rad)
-            generate_row((side_length - 1) + idx, (side_length * 2) - 1 - idx, (x_val, y_val), nodes_set, nodes_dict, True, False)
+            generate_row((side_length - 1) + idx, (side_length * 2) - 1 - idx, (x_val, y_val), nodes_set, nodes_dict, board_array, True, False)
 
 
-def generate_row(row_number, length, starting_spot, nodes_set, nodes_dict, setup_connections, above=None):
+def generate_row(row_number, length, starting_spot, nodes_set, nodes_dict, board_array, setup_connections, above=None):
+    templist = list()
     for idx in range(length):
         temp = HexNode(row_number, idx, starting_spot[0] + idx * RADIUS, starting_spot[1])
         nodes_set.add(temp)
+        templist.append(temp)
         nodes_dict[f"{row_number}{idx}"] = temp
         if setup_connections and above:
             modifiers_above = [(0, -1), (1, 0), (1, 1)]
@@ -71,6 +81,7 @@ def generate_row(row_number, length, starting_spot, nodes_set, nodes_dict, setup
         elif not setup_connections:
             mod = (0, -1)
             adding_neighbors(temp, mod, row_number, idx, nodes_dict)
+    board_array[row_number] = list(templist)
 
 
 def adding_neighbors(temp, mod, row_number, idx, nodes_dict):
@@ -105,12 +116,36 @@ def find_circle(screen, input_location, nodes):
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((1000, 1000))
+    # https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_PyGame_Integration.py
+    layout = [[sg.Button("Choose File", font=('Arial Bold', 12)),
+               sg.Button("New Game From Custom", font=('Arial Bold', 12)),
+               sg.Button("New Game From Default", font=('Arial Bold', 12)),
+               sg.Cancel("Exit Game", font=('Arial Bold', 12))],
+              [sg.Graph((700, 700), (0, 700), (700, 0), key='-GRAPH-', enable_events=True)]]
+
+    window = sg.Window('PySimpleGUI + PyGame', layout, finalize=True)
+    graph = window['-GRAPH-']
+
+    embed = graph.TKCanvas
+    os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+    if platform.system == "Linux":
+        os.environ['SDL_VIDEODRIVER'] = "x11"
+    elif platform.system == "Windows":
+        os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+    screen = pygame.display.set_mode((800, 800))
+
+    while True:
+        event, values = window.read(timeout=10)
+        pygame.display.update()
+        break
+
+    screen.fill(pygame.Color(200, 162, 200))
     clock = pygame.time.Clock()
-    # screen.fill((200, 162, 200))
-    screen.fill((120, 120, 120))
-    nodes = generate_board(6, (100, 500))
+    pygame.display.init()
+    pygame.display.update()
+    board_array = [[]]*(2*BOARD_SIZE-1)
+    nodes = generate_board(BOARD_SIZE, (50, 350), board_array)
 
     for item in nodes[0]:
         for neighbor in item.neighbors.values():
@@ -118,86 +153,22 @@ def main():
 
     for item in nodes[0]:
         pygame.draw.circle(screen, get_random_colour(), (item.screen_x, item.screen_y), CIRC_RAD)
-    print(f"this is the length {len(nodes[0])}")
-
     pygame.display.flip()
-    terminated = False
-    while not terminated:
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                temp = event.dict["pos"]
-                location = [temp[0], temp[1]]
-                find_circle(screen, location, nodes[0])
-            elif event.type == pygame.QUIT:
-                terminated = True
+
+
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit Game'):
+            break
+
+        elif event == '-GRAPH-':
+            temp = values['-GRAPH-']
+            find_circle(screen, [temp[0], temp[1]], nodes[0])
+            pygame.display.flip()
+        pygame.display.update()
 
         clock.tick(50)
     pygame.display.quit()
 
 
-# https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_PyGame_Integration.py
-layout = [[sg.Text('Test of PySimpleGUI with PyGame')],
-          [sg.Graph((800, 800), (800, 0), (0, 800),
-                    background_color='lightblue', key='-GRAPH-', enable_events=True)],
-          [sg.Button('Draw'), sg.Exit()]]
-
-window = sg.Window('PySimpleGUI + PyGame', layout, finalize=True)
-graph = window['-GRAPH-']
-
-embed = graph.TKCanvas
-os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
-if platform.system == "Linux":
-    os.environ['SDL_VIDEODRIVER'] = "x11"
-elif platform.system == "Windows":
-    os.environ['SDL_VIDEODRIVER'] = 'windib'
-
-screen = pygame.display.set_mode((800, 800))
-pygame.display.init()
-pygame.display.update()
-while True:
-    event, values = window.read(timeout=10)
-    pygame.display.update()
-    break
-
-screen.fill(pygame.Color(200, 162, 200))
-clock = pygame.time.Clock()
-pygame.display.init()
-pygame.display.update()
-nodes = generate_board(6, (100, 500))
-
-for item in nodes[0]:
-    for neighbor in item.neighbors.values():
-        pygame.draw.line(screen, (0, 0, 0), (item.screen_x, item.screen_y), (neighbor.screen_x, neighbor.screen_y))
-
-for item in nodes[0]:
-    pygame.draw.circle(screen, get_random_colour(), (item.screen_x, item.screen_y), CIRC_RAD)
-print(f"this is the length {len(nodes[0])}")
-
-pygame.display.flip()
-
-
-while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    print(type(event))
-    print(type(values))
-    if event in (sg.WIN_CLOSED, 'Exit'):
-        print("tried to quit uwu")
-        #pygame.display.quit()
-        break
-    
-    elif event == '-GRAPH-':
-
-        temp = values['-GRAPH-']
-        print(temp)
-        print(type(temp))
-        location = [temp[1], temp[0]]
-        print(location)
-        print('lets go')
-        find_circle(screen, location, nodes[0])
-        pygame.display.flip()
-    pygame.display.update()
-
-    clock.tick(50)
-pygame.display.quit()
+main()
